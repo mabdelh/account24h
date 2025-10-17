aa2c1b722df213ad69e01871fd002aa2d60f273e
mabdelh <47225659+mabdelh@users.noreply.github.com>
2025-10-17 16:21:20 +0200

    Update token.js

--- token.js
+++ token.js
@@ -15,4 +15,34 @@ export default [
         },
         selfMute: true,
     },
+    {
+        channelId: "1292842847216734280",
+        serverId: "762786136459968521",
+        token: process.env.token2,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "idle",
+        },
+        selfMute: true,
+    },
+    {
+        channelId: "1292842847216734280",
+        serverId: "762786136459968521",
+        token: process.env.token3,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "idle",
+        },
+        selfMute: true,
+    },    
 ];
